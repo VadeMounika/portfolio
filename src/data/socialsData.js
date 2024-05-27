8639abf37e130a87df3ac64927cda58c454e9718
VadeMounika <vademounika@Vades-MacBook-Air.local>
2024-05-27 02:08:14 -0400

    sec

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,11 +1,11 @@
 export const socialsData = {
-  github: "https://github.com/nandu208",
+  github: "https://github.com/VadeMounika",
   facebook: "https://www.facebook.com/",
   linkedIn:
-    "https://www.linkedin.com/in/nanda-kishore-reddy-yerrabandi-a61516141/",
-  instagram: "https://www.instagram.com/nandu070/",
+    "https://www.linkedin.com/in/mounika-vade-42b767168/",
+  instagram: "https://www.instagram.com/",
   codepen: "https://codepen.io/",
-  twitter: "https://twitter.com/NandaKi03893787",
+  twitter: "https://twitter.com/",
   reddit: "https://www.reddit.com/user/",
   blogger: "https://www.blogger.com/",
   medium: "https://medium.com/@",
